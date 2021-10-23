14cea3e81f29449c073fc50d45fb17a1ef12cfc7
alex-hobby <alexhobby98@gmail.com>
2021-10-23 02:57:55 -0400

    Connect React App to Flask server

--- flaskapi/api.py
+++ flaskapi/api.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 @app.route('/test')
 def do_something():
     print('hello')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
